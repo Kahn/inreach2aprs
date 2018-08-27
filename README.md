df10271260d7464ad6526b9242b49e6a8cf276e8
Sam Wilson <vk2gpl@wia.org.au>
2018-08-27 22:44:01 +1000

    Initial proof of concept for inreach2aprs

--- README.md
+++ README.md
@@ -6,12 +6,33 @@ For reference - The Delorme API is documented at https://files.delorme.com/suppo
 
 ## Goal
 
-Send inReach positions to APRSIS
+Send inReach positions to APRSIS ✅
+
+      usage: inreach2aprs.py [-h] [--mapshare_url MAPSHARE_URL]
+                             [--mapshare_password MAPSHARE_PASSWORD]
+                             aprs_callsign aprs_ssid aprs_password
+
+      inreach2aprs
+
+      positional arguments:
+        aprs_callsign         Your callsign; VK2GPL
+        aprs_ssid             ARPS SSID or none; eg "-6"; see
+                              http://www.aprs.net.au/general/standard-ssids/
+        aprs_password         APRS Passcode
+
+      optional arguments:
+        -h, --help            show this help message and exit
+        --mapshare_url MAPSHARE_URL
+                              inReach MapShare URL; see
+                              https://support.garmin.com/en-
+                              AU/?faq=p2lncMOzqh71P06VifrQE7
+        --mapshare_password MAPSHARE_PASSWORD
+                              OPTIONAL - inReach MapShare password
 
 ## Nice to haves
 
 * Support public MapShare links
-* Support authenticated MapShare links
+* Support authenticated MapShare links ✅
 * Handle deduplicating rather than relying on APRSIS to handle dupes
 
 ## Theory
--- inreach2aprs.py
+++ inreach2aprs.py
@@ -1,3 +1,97 @@
 #!/usr/bin/env python3
 
-# One day I'll be a real bit o'code.
+# It's super dodgy but this will give you something to run via cron at least with a zero return for success ...
+
+import os, sys
+import aprslib
+import datetime
+import LatLon23
+import pprint
+import argparse
+import requests
+# Requires hacks from http://installfights.blogspot.com/2018/04/how-to-run-pykml-in-python3.html
+from pykml import parser as kmlparser
+from urllib.parse import urlparse
+from io import BytesIO
+
+parser = argparse.ArgumentParser(description='inreach2aprs')
+# default=os.environ.get('I2A', None)
+parser.add_argument('aprs_callsign', help='Your callsign; VK2GPL')
+parser.add_argument('aprs_ssid', help='ARPS SSID or none; eg "-6"; see http://www.aprs.net.au/general/standard-ssids/')
+parser.add_argument('aprs_password', help='APRS Passcode')
+parser.add_argument('--mapshare_url', help='inReach MapShare URL; see https://support.garmin.com/en-AU/?faq=p2lncMOzqh71P06VifrQE7')
+parser.add_argument('--mapshare_password', help='OPTIONAL - inReach MapShare password')
+
+args = parser.parse_args()
+if not args.aprs_callsign:
+    exit(parser.print_usage())
+
+
+pp = pprint.PrettyPrinter()
+o = urlparse(args.mapshare_url)
+# print(o.path.strip("/"), args.mapshare_password)
+
+try:
+    r = requests.get(args.mapshare_url, auth=(o.path.strip("/"), args.mapshare_password))
+    r.raise_for_status()
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+    raise
+
+kml = kmlparser.parse(BytesIO(r.content)).getroot()
+
+# Maps as per https://files.delorme.com/support/inreachwebdocs/KML%20Feeds.pdf and with inreach.kml example
+#0 - Id int
+#1 - Time UTC str
+#2 - Time str
+#3 - Name str
+#4 - Map Display Name str
+#5 - Device Type str
+#6 - IMEI str
+#7 - Incident Id str
+#8 - Latitude float
+#9 - Longitude float
+#10 - Elevation str
+#11 - Velocity str
+#12 - Course str
+#13 - Valid GPS Fix bool
+#14 - In Emergency bool
+#15 - Text str
+#16 - Event str
+
+d = datetime.datetime.strptime(str(kml.Document.Folder.Placemark[0].TimeStamp.when),'%Y-%m-%dT%H:%M:%SZ')
+aprs_timestamp = d.strftime("%d%H%Mz")
+
+inreach_lat = round(float(kml.Document.Folder.Placemark[0].ExtendedData.Data[8].value),2)
+# print(inreach_lat)
+inreach_lon = round(float(kml.Document.Folder.Placemark[0].ExtendedData.Data[9].value),2)
+# print(inreach_lon)
+
+aprs_pos = LatLon23.LatLon( LatLon23.Latitude(inreach_lat), LatLon23.Longitude(inreach_lon))
+
+lat_deg, lon_deg = aprs_pos.to_string('d%')
+lat_mins, lon_mins = aprs_pos.to_string('M%')
+lat_hem, lon_hem = aprs_pos.to_string('H%')
+aprs_lat_mins = format(float(lat_mins),'.2f')
+aprs_lon_mins = format(float(lon_mins),'.2f')
+
+try:
+    aprs_lat = lat_deg.replace("-","") + aprs_lat_mins + lat_hem
+    # print(aprs_lat, len(aprs_lat))
+    aprs_lon = lon_deg.replace("-","") + aprs_lon_mins + lon_hem
+    # print(aprs_lon, len(aprs_lon))
+    assert len(aprs_lat)<=8
+    assert len(aprs_lat)<=9
+except AssertionError:
+    print("Position report values exceed specification length")
+
+position_report = args.aprs_callsign + args.aprs_ssid + ">"+ "APZ001,TCPIP*:/" + aprs_timestamp + aprs_lat + "/" + aprs_lon + "Sinreach2aprs-0.0.1"
+# pp.pprint(aprslib.parse(position_report))
+
+try:
+    AIS = aprslib.IS(args.aprs_callsign, passwd=args.aprs_password, port=14580)
+    AIS.connect()
+    AIS.sendall(position_report)
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+    raise
